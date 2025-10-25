79c7a404d67e972d2c8d2c37df9f1846a11d603d
Emadelddin_ <emadelddin@beautinda.com>
2025-10-25 11:40:09 +0200

    Implement Homework B01 Name class

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -19,17 +19,32 @@ export class Name {
     private components: string[] = [];
 
     /** Expects that all Name components are properly masked */
-    constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+    // @methodtype constructor
+    constructor(other: string[], delimiter: string = DEFAULT_DELIMITER) {
+        this.delimiter = delimiter;
+        this.components = [...other];
     }
-
+    /**
+     * Checks whether the given index is within bounds for the components array.
+     * Throws an Error if the index is invalid.
+     * @param i - The index to check
+     * @param allowEnd - If true, allows index equal to components.length (useful for insert at end)
+     */
+     // @methodtype private-method
+   private checkIndex(i: number, allowEnd: boolean = false): void {
+        const max = allowEnd ? this.components.length : this.components.length - 1;
+        if (i < 0 || i > max) {
+            throw new Error(`Index ${i} out of bounds`);
+        }
+    }
     /**
      * Returns a human-readable representation of the Name instance using user-set control characters
      * Control characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
+      // @methodtype get-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+       return this.components.join(delimiter);
     }
 
     /** 
@@ -37,36 +52,45 @@ export class Name {
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The control characters in the data string are the default characters
      */
+     // @methodtype get-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
+       return this.components.join(DEFAULT_DELIMITER);
+     }
+    // @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        this.checkIndex(i);
+        return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
+     // @methodtype set-method
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.checkIndex(i);
+        this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
+     // @methodtype get-method
      public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype set-method
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.checkIndex(i, true);
+        this.components.splice(i, 0, c);
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype set-method
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
-
+    // @methodtype set-method
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
-    }
+        this.checkIndex(i);
+        this.components.splice(i, 1);
+     }
 
 }
